063d4d438722cf535192113c5d86888c07fe1481
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-18 13:42:55 -0800

    all mvp goals in place

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,13 @@
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
+
+
+page('/', articlesController.index);
+// page('new', new);
+// page('admin', admin);
+page('/about', aboutController.index);
+
+page();
+
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
