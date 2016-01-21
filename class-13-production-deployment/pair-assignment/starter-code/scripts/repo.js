3c815a8f41f7debac64e172d5d86a37b7813b68f
Michael Sullivan <sapientia.veritas@gmail.com>
2016-01-20 21:05:48 -0800

    achieved todos

--- class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
+++ class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
@@ -3,18 +3,15 @@
 
   repos.all = [];
 
-  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
+    $.get({
+      url: '/github/users/scotist/repos' +
+        '?per_page=100' + '&sort=updated',
+    })
+    .done(function(data, message, xhr) {
+      repos.all = data;
+    })
+    .done(callback);
   };
 
   repos.with = function(attr) {
